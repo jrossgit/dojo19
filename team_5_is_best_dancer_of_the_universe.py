f2cb3829608cf9176890f30917bb9da7829f3055
Damian Świstowski <damian@swistowski.org>
2019-09-15 21:00:11 +0100

    lets dance refactored

--- team_5_is_best_dancer_of_the_universe.py
+++ team_5_is_best_dancer_of_the_universe.py
@@ -9,9 +9,22 @@ from cocos.layer import Layer
 from cocos.scene import Scene
 from cocos.sprite import Sprite
 
+def left(speed: float):
+    return ((MoveBy((100, 0), speed), Delay(speed)), (Delay(speed), MoveBy((100, 0), speed)))
 
-class HelloWorld(Layer):
-    def __init__(self):
+
+def right(speed: float):
+    return ((Delay(speed), MoveBy((-100, 0), speed)),
+     (MoveBy((-100, 0), speed), Delay(speed)))
+
+def front(speed: float):
+    return ((Delay(speed), MoveBy((0, 100), speed)), (MoveBy((0, 100), speed), Delay(speed)))
+
+def back(speed: float):
+    return ((Delay(speed), MoveBy((0, -100), speed)), (MoveBy((0, -100), speed), Delay(speed)))
+
+class LetsDance(Layer):
+    def __init__(self, sequence, bpm):
         super().__init__()
         self.left_foot = Sprite("./images/flat-black-l.png")
         self.right_foot = Sprite("./images/flat-black-r.png")
@@ -21,17 +34,8 @@ class HelloWorld(Layer):
         self.add(self.left_foot)
         self.add(self.right_foot)
 
-        speed = 0.5
-
-        left = ((MoveBy((100, 0), speed), Delay(speed)), (Delay(speed), MoveBy((100, 0), speed)))
-
-        right = ((Delay(speed), MoveBy((-100, 0), speed)), (MoveBy((-100, 0), speed), Delay(speed)))
-
-        front = ((Delay(speed), MoveBy((0, 100), speed)), (MoveBy((0, 100), speed), Delay(speed)))
-
-        back = ((Delay(speed), MoveBy((0, -100), speed)), (MoveBy((0, -100), speed), Delay(speed)))
-
-        dance = [left, right, front, back]
+        speed = 60 / bpm
+        dance = [s(speed) for s in sequence]
 
         right_leg_sequence = chain(*(step[0] for step in dance))
         left_leg_sequence = chain(*(step[1] for step in dance))
@@ -39,14 +43,7 @@ class HelloWorld(Layer):
         self.right_foot.do(Repeat(sum(right_leg_sequence, Delay(0))))
         self.left_foot.do(Repeat(sum(left_leg_sequence, Delay(0))))
 
-    def on_key_press(self, key, modifiers):
-        print("key pressed")
-        self.left_foot.do(RotateBy(360, duration=3))
-
 
 if __name__ == "__main__":
-    print("start")
     director.init()
-    layer = HelloWorld()
-    scene = Scene(layer)
-    director.run(scene)
+    director.run(Scene(LetsDance([left, right, front, back], 120)))

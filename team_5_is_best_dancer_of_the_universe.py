d77bb08c5c6099ce5f56ffec3aa70ca91751b32d
Damian Świstowski <damian@swistowski.org>
2019-09-15 20:53:26 +0100

    faster dance

--- team_5_is_best_dancer_of_the_universe.py
+++ team_5_is_best_dancer_of_the_universe.py
@@ -1,12 +1,7 @@
-import random
-from enum import Enum
-from enum import auto
-from typing import NamedTuple
+from itertools import chain
 
 from cocos.actions import Delay
-from cocos.actions import Move
 from cocos.actions import MoveBy
-from cocos.actions import MoveTo
 from cocos.actions import Repeat
 from cocos.actions import RotateBy
 from cocos.director import director
@@ -15,53 +10,34 @@ from cocos.scene import Scene
 from cocos.sprite import Sprite
 
 
-class Leg(Enum):
-    LEFT = auto()
-    RIGHT = auto()
-
-
-class DanceMove(NamedTuple):
-    dx: int
-    dy: int
-    # speed: int
-    leg: Leg
-
-
-#left_leg_up = DanceMove(0, 10, Leg.LEFT)
-# left_leg_up = DanceMove(0, 10, Leg.LEFT)
-
-
-
 class HelloWorld(Layer):
     def __init__(self):
         super().__init__()
-        self.left_foot = Sprite('./images/flat-black-l.png')
-        self.right_foot = Sprite('./images/flat-black-r.png')
+        self.left_foot = Sprite("./images/flat-black-l.png")
+        self.right_foot = Sprite("./images/flat-black-r.png")
 
         self.left_foot.position = 200, 200
         self.right_foot.position = 300, 200
         self.add(self.left_foot)
         self.add(self.right_foot)
-        #while True:
-        # leg = random.choice((self.left_foot, self.right_foot))
 
-        right = [
-            MoveBy((100, 0), 1),
-            Delay(1),
-            MoveBy((-100, 0), 1),
-            Delay(1)
-        ]
-        left = [
-            Delay(1),
-            MoveBy((100, 0), 1),
-            Delay(1),
-            MoveBy((-100, 0), 1)
-        ]
+        speed = 0.5
 
-        self.right_foot.do(Repeat(sum(right, Delay(0))))
-        self.left_foot.do(Repeat(sum(left, Delay(0))))
+        left = ((MoveBy((100, 0), speed), Delay(speed)), (Delay(speed), MoveBy((100, 0), speed)))
 
-        # self.left_foot.do(RotateBy(360, duration=3))
+        right = ((Delay(speed), MoveBy((-100, 0), speed)), (MoveBy((-100, 0), speed), Delay(speed)))
+
+        front = ((Delay(speed), MoveBy((0, 100), speed)), (MoveBy((0, 100), speed), Delay(speed)))
+
+        back = ((Delay(speed), MoveBy((0, -100), speed)), (MoveBy((0, -100), speed), Delay(speed)))
+
+        dance = [left, right, front, back]
+
+        right_leg_sequence = chain(*(step[0] for step in dance))
+        left_leg_sequence = chain(*(step[1] for step in dance))
+
+        self.right_foot.do(Repeat(sum(right_leg_sequence, Delay(0))))
+        self.left_foot.do(Repeat(sum(left_leg_sequence, Delay(0))))
 
     def on_key_press(self, key, modifiers):
         print("key pressed")

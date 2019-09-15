866220aaf000e75b8c1be5410152ea13ff020754
Thomas Kluyver <thomas@kluyver.me.uk>
2019-09-15 20:58:41 +0100

    SOMETHING!

--- team_0xc/play_moves.py
+++ team_0xc/play_moves.py
@@ -8,6 +8,7 @@ from __future__ import division, print_function, unicode_literals
 # This code is so you can run the samples without installing the package
 import sys
 import os
+import json 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 #
 
@@ -15,6 +16,18 @@ import cocos
 from cocos.actions import *
 
 # A color layer  is a Layer with the a color attribute
+
+def make_person(position):
+    x, y = position
+    left = cocos.sprite.Sprite('images/left_arm_sprite.png', scale=0.15, position=(x-40, y))
+    right = cocos.sprite.Sprite('images/arm_sprite.png', scale=0.15, position=(x+40, y))
+    head = cocos.sprite.Sprite('images/head_sprite.jpg', scale=0.15, position=(x, y+30))
+
+    return {
+        'left_arm': left,
+        'right_arm': right,
+        'head': head,
+    }
 
 
 class HelloWorld(cocos.layer.ColorLayer):
@@ -37,29 +50,14 @@ class HelloWorld(cocos.layer.ColorLayer):
         # similar to cocos.text.Label, a cocos.sprite.Sprite
         # is a subclass of pyglet.sprite.Sprite with the befits of
         # being a CocosNode.
-        person = {
-            'left': cocos.sprite.Sprite('images/arm_sprite.png'),
-            'right': cocos.sprite.Sprite('images/arm_sprite.png'),
-            'head': cocos.sprite.Sprite('images/head_sprite.jpg')
-        }
-
-        print(person)
-
-        # sprite in the center of the screen (default is 0,0)
-        person['left'].position = 320, 240
-        person['right'].position = 420, 240
-        person['head'].position = 370, 260
-
-        # sprite scale attribute starts with 3 (default 1 )
-        person['left'].scale = 0.2
-        person['right'].scale = 0.2
-        person['head'].scale = 0.2
+        people = [make_person(((110 * i) + 100, 100 + (50 * i))) for i in range(5)]
 
         # add the sprite as a child, but with z=1 (default is z=0).
         # this means that the sprite will be drawn on top of the label
-        self.add(person['left'], z=1)
-        self.add(person['right'], z=1)
-        self.add(person['head'], z=1)
+        for person in people:
+            self.add(person['left_arm'], z=1)
+            self.add(person['right_arm'], z=1)
+            self.add(person['head'], z=1)
 
         # create a ScaleBy action that lasts 2 seconds
         # scale = ScaleBy(3, duration=2)
@@ -71,6 +69,17 @@ class HelloWorld(cocos.layer.ColorLayer):
         # sprite.do(Repeat(Reverse(scale) + scale))
 
 if __name__ == "__main__":
+    with open('basic_moves.json', 'r') as fd: 
+        move_json = json.loads(fd.read())
+    #    print(move_json)
+    #    print(type(move_json))
+
+    actions = {
+        ("UP", "UP"): RotateBy(180, duration=10),
+        ("DOWN", "UP"): RotateBy(-180, duration=10)
+    }
+
+
     # director init takes the same arguments as pyglet.window
     cocos.director.director.init()
 
